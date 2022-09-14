55443baad8a134139c1cc43f85902247d5ec8b37
IaroslavaDubitkaia <70658590+IaroslavaDubitkaia@users.noreply.github.com>
2022-09-14 10:19:26 +0300

    Add files via upload

---
+++ pr3.py
@@ -0,0 +1,11 @@
+a = input("introduceti numarul ")
+b = input("introduceti numarul ")
+c = input("introduceti numarul ")
+d = input("introduceti numarul ")
+def adunareafractiei():
+    return ((a*d)+(c*b))/(b*d)
+print("rezultatul adunarii = ", adunareafractiei())
+
+def inmultireafractiei():
+    return (a*c)/(b*d) 
+print("rezultatul inmultirii = ", inmultireafractiei())
